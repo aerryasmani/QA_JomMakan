69133f75d5d85ef2b252a2b28cd55fd6756254fc
Aerry Asmani <aerryasmani@gmail.com>
2025-10-23 01:40:21 +0800

    Refactored homepage.spec.js with fixtures and helper functions

--- tests/TestSuite/homepage.spec.js
+++ tests/TestSuite/homepage.spec.js
@@ -3,8 +3,25 @@ import { test, expect } from '@playwright/test';
 const BaseURL = 'https://jommakan.app';
 const PageTitle = 'JomMakan! - Makan Apa Hari Ni?';
 
+
+//------- Helper Function for URL ------//
 async function VerifyPageTitle(page) {
   await expect(page).toHaveTitle(PageTitle);
+}
+
+
+//------- Helper Function for Buttons ------//
+async function VerifyButton(page, buttonName) {
+  const button = page.getByRole('button', { name: buttonName });
+  await expect(button).toBeVisible();
+  await expect(button).toHaveText(buttonName);
+  await button.click();
+}
+
+async function CloseButton(page) {
+  const button = page.getByRole('button', { name: 'Back' });
+  await expect(button).toBeVisible();
+  await button.click();
 }
 
 // Setup
@@ -13,27 +30,23 @@ test.beforeEach(async ({ page }) => {
 });
 
 
-// Test Cases
+/*
+//------- Test Cases ------//
 test('CF-001 | Verify homepage loads correctly', async ({ page }) => {
   await VerifyPageTitle(page);
 });
 
-/*
 test  ("CF-002| Verify the Logo Is Present", async ({ page }) => {  
-  await page.goto(BaseURL);
-  await expect(page).toHaveTitle('JomMakan! - Makan Apa Hari Ni?');
+  await expect(page).toHaveTitle(PageTitle);
 
-  // Expect a title "to contain" a substring.
   const Header1 = page.locator('h1');
   await expect(Header1).toBeVisible();
   await expect(Header1).toHaveText('JomMakan!')
 });
 
 test  ("CF-003| Verify the hero header content text is present", async ({ page }) => {  
-  await page.goto(BaseURL);
-  await expect(page).toHaveTitle('JomMakan! - Makan Apa Hari Ni?');
+  await VerifyPageTitle(page);
 
-    // Expect a title "to contain" a substring.
   const Header2 = page.locator('h2');
   await expect(Header2).toBeVisible();
   await expect(Header2).toHaveText('Makan Apa Hari Ni?')
@@ -44,62 +57,39 @@ test  ("CF-003| Verify the hero header content text is present", async ({ page }
 
 });
 
+
+
 test  ("CF-004| Verify the 'Dapur Time' button is present and visible", async ({ page }) => {  
-  await page.goto(BaseURL);
+  
+  await VerifyButton(page, 'Dapur Time')
 
-  const Btn_DapurTime = page.getByRole('button', { name: 'Dapur Time' })
-  await expect(Btn_DapurTime).toBeVisible({ timeout: 10000 });
-  await expect(Btn_DapurTime).toHaveText('Dapur Time');
-  // Click it and verify navigation or change in page content
-  await Btn_DapurTime.click();
+  const DapurCategory = ['Sarapan', 'Makan Tengahari', 'Makan Malam'];
+  for (const category of DapurCategory) {
+    const categoryBtn = page.getByRole('button', { name: category });
+    await expect(categoryBtn, `Button ${category} should be visible`).toBeVisible();
+    await expect(categoryBtn).toHaveText(category);
+  }
 
-  // Verify the new page content after clicking the button is present
-  await expect(page.getByText('Pilih Kategori')).toBeVisible();
+  await CloseButton(page);
 
-  //Verify the 'Sarapan' Button is present
-  const Btn_Sarapan = page.getByRole('button', { name: 'Sarapan' })
-  await expect(Btn_Sarapan).toBeVisible({ timeout: 10000 });
-  await expect(Btn_Sarapan).toHaveText('Sarapan');
-
-  //Verify the 'Makan Tengahari' Button is present
-  const Btn_MakanTengahari = page.getByRole('button', { name: 'Makan Tengahari' })
-  await expect(Btn_MakanTengahari).toBeVisible({ timeout: 10000 });
-  await expect(Btn_MakanTengahari).toHaveText('Makan Tengahari');
-
-  //Verify the 'Makan Malam' Button is present
-  const Btn_Malam = page.getByRole('button', { name: 'Makan Malam' })
-  await expect(Btn_Malam).toBeVisible({ timeout: 10000 });
-  await expect(Btn_Malam).toHaveText('Makan Malam');
-
+  const Header2 = page.locator('h2');
+  await expect(Header2).toHaveText('Makan Apa Hari Ni?');
 });
 
-
-test  ("CF-005| Verify the 'Tapau' button is present and visible", async ({ page }) => {  
-  await page.goto(BaseURL);
-
-  const Btn_DapurTime = page.getByRole('button', { name: 'Tapau' })
-  await expect(Btn_DapurTime).toBeVisible({ timeout: 10000 });
-  await expect(Btn_DapurTime).toHaveText('Tapau');
-  // Click it and verify navigation or change in page content
-  await Btn_DapurTime.click();
-
-  // Verify the new page content after clicking the button is present
-  await expect(page.getByText('Pilih Kategori')).toBeVisible();
-
-  //Verify the 'Sarapan' Button is present
-  const Btn_Sarapan = page.getByRole('button', { name: 'Nasi' })
-  await expect(Btn_Sarapan).toBeVisible({ timeout: 10000 });
-  await expect(Btn_Sarapan).toHaveText('Nasi');
-
-  //Verify the 'Makan Tengahari' Button is present
-  const Btn_MakanTengahari = page.getByRole('button', { name: 'Mee' })
-  await expect(Btn_MakanTengahari).toBeVisible({ timeout: 10000 });
-  await expect(Btn_MakanTengahari).toHaveText('Mee');
-
-  //Verify the 'Makan Malam' Button is present
-  const Btn_Malam = page.getByRole('button', { name: 'Roti Wrap' })
-  await expect(Btn_Malam).toBeVisible({ timeout: 10000 });
-  await expect(Btn_Malam).toHaveText('Roti Wrap');
-
-});
 */
+test  ("CF-005| Verify the 'Tapau' button is present and visible", async ({ page }) => {  
+  await VerifyButton(page, 'Tapau')
+
+  const TapauCategory = ['Nasi', 'Mee', 'Roti Wrap', 'Sepinggan', 'Bakar Goreng', 'Ringan'];
+  for (const category of TapauCategory) {
+    const categoryBtn = page.getByRole('button', { name: category });
+    await expect(categoryBtn, `Button ${category} should be visible`).toBeVisible();
+    await expect(categoryBtn).toHaveText(category);
+  }
+
+  await CloseButton(page);
+
+  const Header2 = page.locator('h2');
+  await expect(Header2).toHaveText('Makan Apa Hari Ni?');
+
+});
